e40c6aecc6d7c9150b940ea5cb2d69cf419e8922
sarion74 <m.bontron@gmail.com>
2015-02-24 20:22:15 +0100

    Version "plus à jour"

--- RS_2016/app/src/main/java/com/example/sarion/rs_2016/MainActivity.java
+++ RS_2016/app/src/main/java/com/example/sarion/rs_2016/MainActivity.java
@@ -1,18 +1,74 @@
 package com.example.sarion.rs_2016;
 
+//Import of the libraries
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuItem;
-import android.util.DisplayMetrics;
+import android.view.MenuItem ;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.view.View;
 
 
 public class MainActivity extends ActionBarActivity {
+    //Declaration of the different elements
+    private Button      buttonConnexionCM ;
+    private Button      buttonDeconnexionCM ;
+    private Button      buttonReccord ;
+    private Button      buttonWalk ;
+    private Button      buttonMove ;
+    private Button      buttonSend ;
+
+    private EditText    editTextIPOctect1CM ;
+    private EditText    editTextIPOctect2CM ;
+    private EditText    editTextIPOctect3CM ;
+    private EditText    editTextIPOctect4CM ;
+    private EditText    editTextIPOctect1Robot ;
+    private EditText    editTextIPOctect2Robot ;
+    private EditText    editTextIPOctect3Robot ;
+    private EditText    editTextIPOctect4Robot ;
+    private EditText    editTextXVal ;
+    private EditText    editTextYVal ;
+    private EditText    editTextAngleVal ;
+
+    private ListView    robotList ;
+    private ListView    featuresList ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Set links for the buttons
+        this.buttonConnexionCM      = (Button)this.findViewById(R.id.buttonConnexionCM) ;
+        this.buttonDeconnexionCM    = (Button)this.findViewById(R.id.buttonDeconnexionCM) ;
+        this.buttonMove             = (Button)this.findViewById(R.id.buttonMove) ;
+        this.buttonReccord          = (Button)this.findViewById(R.id.buttonReccord) ;
+        this.buttonSend             = (Button)this.findViewById(R.id.buttonSend);
+        this.buttonWalk             = (Button)this.findViewById(R.id.buttonWalk) ;
+
+        //Set links for the texts
+        this.editTextAngleVal       = (EditText)this.findViewById(R.id.editTextThetaVal) ;
+        this.editTextXVal           = (EditText)this.findViewById(R.id.editTextXVal) ;
+        this.editTextYVal           = (EditText)this.findViewById(R.id.editTextYVal) ;
+
+        this.editTextIPOctect1CM    = (EditText)this.findViewById(R.id.ipCMOctet1) ;
+        this.editTextIPOctect2CM    = (EditText)this.findViewById(R.id.ipCMOctet2) ;
+        this.editTextIPOctect3CM    = (EditText)this.findViewById(R.id.ipCMOctet3) ;
+        this.editTextIPOctect4CM    = (EditText)this.findViewById(R.id.ipCMOctet4) ;
+
+        this.editTextIPOctect1Robot = (EditText)this.findViewById(R.id.editTextIPRobot1) ;
+        this.editTextIPOctect2Robot = (EditText)this.findViewById(R.id.editTextIPRobot2) ;
+        this.editTextIPOctect3Robot = (EditText)this.findViewById(R.id.editTextIPRobot3) ;
+        this.editTextIPOctect4Robot = (EditText)this.findViewById(R.id.editTextIPRobot4) ;
+
+        //Set links for the lists
+        this.robotList              = (ListView)this.findViewById(R.id.listViewRobot) ;
+        this.featuresList           = (ListView)this.findViewById(R.id.listViewFeatures) ;
+
+        //Set the click listeners for the buttons
     }
 
 
